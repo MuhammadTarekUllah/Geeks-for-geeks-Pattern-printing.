a4a8066c51d2b857d13865c6b69421e17dc6fe8b
MuhammadTarekUllah <142986550+MuhammadTarekUllah@users.noreply.github.com>
2023-09-09 13:48:43 +0100

    Add files via upload

---
+++ C Program to Print Full Diamond Shape Pyramid.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+  
+void printDiamond(int n)
+{
+    int space = n - 1;
+  
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < space; j++)
+            printf(" ");
+  
+        for (int j = 0; j <= i; j++)
+            printf("* ");
+  
+        printf("\n");
+        space--;
+    }
+  
+    space = 0;
+  
+    for (int i = n; i > 0; i--) 
+    {
+        for (int j = 0; j < space; j++)
+            printf(" ");
+  
+        for (int j = 0; j < i; j++)
+            printf("* ");
+  
+        printf("\n");
+        space++;
+    }
+}
+  
+int main()
+{
+    int rows;
+    printf("Enter number of rows for pattern printing: ");
+    scanf("%d",&rows);
+    printDiamond(rows);
+    return 0;
+}
